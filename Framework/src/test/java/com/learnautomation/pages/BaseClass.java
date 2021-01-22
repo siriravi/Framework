04df61467d2a20c11dcef443829e094018f53776
Ravi Siripurapu <ravieee1@gmail.com>
2021-01-22 22:34:04 +0530

    Latest Updates

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -44,13 +45,14 @@ public class BaseClass {
 		
 	}
 	
-	
+	@Parameters({"browser","urltobeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String browser, String url)
 	{
 		Reporter.log("Trying to start browser and getting application ready", true);
 		
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getAppURL());
+		driver = BrowserFactory.startApplication(driver, browser, url);
+		//driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getAppURL());
 	}
 	
 	
